f227ba5a7d07f6b7791ab89f86f5f9ae5b47367c
Ygor Is <96506991+Ygor-Is@users.noreply.github.com>
2021-12-23 11:53:46 -0400

    Add files via upload

---
+++ TowshipDB.lua
@@ -0,0 +1,47 @@
+--#-----------------------------------------#
+
+   -- creditos:
+   -- ➲ YoTube : https://bit.ly/3qnULwt ➲ 
+   -- ➲ gitHub : https://bit.ly/3H9OHy2 ➲
+   -- ➲ WebSite: https://bit.ly/3qnUOZb ➲
+   -- endCredts
+
+--#-----------------------------------------#
+
+--# Skins
+skinsdb = {
+  { name="➲ Estação Discoteca",value1="1768641322",value2="1918132078",value3="1399744865",value4="1769234804",value5="1398763119",value6="14416",value7="1",flagsOn="1" },
+  { name="➲ Skin Trem Discoteca",value1="1768641308",value2="1918132078",value3="1601071457",value4="3690579",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Trem Vaqueiro",value1="1768641316",value2="1918132078",value3="1601071457",value4="1953719671",value5="7238245",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Espaçoporto",value1="1768641316",value2="1765891950",value3="1919905906",value4="1886609268",value5="6644577",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Veículo de lançamento",value1="1768641318",value2="1765891950",value3="1634496626",value4="1935631726",value5="1701011824",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Trem do Futuro",value1="1768641308",value2="1918132078",value3="1601071457",value4="3297363",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Trem de Natal 2021",value1="1768641320",value2="1918132078",value3="1601071457",value4="1769105507",value5="1634563187",value6="115",value7="1",flagsOn="1"},
+  { name="➲ Trem fantasma",value1="1768641308",value2="1918132078",value3="1601071457",value4="3493971",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Foto da ovelha",value1="1635148044",value2="3420210",value3="0",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Foto do gatinho",value1="1635148044",value2="3158578",value3="0",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Voltar",value1="0",value2="0",value3="0",value4="0",value5="0",value6="0",value7="0",flagsOn="0"}
+}
+
+decoracaodb = {
+  { name="➲ Cafe De verão",value1="1836405526",value2="1601332589",value3="1701208387",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Primeira Prancha",value1="1920291606",value2="1699897190",value3="1818326126",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Quishot de Vitamina",value1="1802064912",value2="1631739753",value3="114",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Casa de praia",value1="1952536352",value2="1967288933",value3="1818322798",value4="1347647343",value5="57",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Coral Natalino",value1="1919435552",value2="1836348265",value3="1130328929",value4="1970433896",value5="115",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Boneco de neve gigante",value1="1634289434",value2="1398764654",value3="1836543854",value4="28257",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Escritório do papai Noel",value1="1851872030",value2="1381982580",value3="1885692773",value4="1852795252",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Casa de biscoito gigante",value1="1852393240",value2="1601332583",value3="1937076040",value4="101",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Chef caranguejo",value1="1634878226",value2="1866686306",value3="27503",value4="0",value5="0",value6="0",value7="1",flagsOn="1"},
+  { name="➲ Voltar",value1="0",value2="0",value3="0",value4="0",value5="0",value6="0",value7="0",flagsOn="0"}
+}
+
+itensdb = {
+  { name="➲ Notas",value1="1935762184",value2="104",value3="0",value4="0",value5="0",value6="0",value7="1000",flagsOn="1" },
+  { name="➲ Tarefas imediatas",value1="1599099672",value2="1885956947",value3="1701669204",value4="114",value5="0",value6="0",value7="1",flagsOn="10"},
+  { name="➲ Vagões",value1="1599099682",value2="1936682818",value3="1634882676",value4="1632464489",value5="29556",value6="0",value7="30",flagsOn="1"},
+  { name="➲ Campos produtivos",value1="1869562406",value2="1884517491",value3="1432642917",value4="1918978160",value5="1953719670",value6="0",value7="100",flagsOn="1"},
+  { name="➲ Cartão De carga",value1="1970225964",value2="1282305904",value3="1415864687",value4="1852399986",value5="1886546241",value6="7631471",value7="100",flagsOn="1"},
+  { name="➲ Balde de tinta",value1="1767993366",value2="1699902574",value3="1952533860",value4="0",value5="0",value6="0",value7="100",flagsOn="1"},
+  { name="➲ Voltar",value1="0",value2="0",value3="0",value4="0",value5="0",value6="0",value7="0",flagsOn="0"}
+}
